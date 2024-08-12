988532397873c81755e885dc283c1d4e8f4cd172
eya chaouechi <eyachaouechi@gmail.com>
2024-08-12 16:44:02 +0100

    Initial commit

---
+++ Assets/btnFn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFn : MonoBehaviour
+{
+
+    public void buttonFunction(string buttonLink)
+    {
+        Application.OpenURL(buttonLink);
+    }
+}
